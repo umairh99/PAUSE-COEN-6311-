69322ce0ef82518e3c8270bd02e2bbf7ccb8a022
Umair <umair.hussain6606@outlook.com>
2024-03-30 10:38:14 -0400

    Frontend

--- BE_Booking/urls.py
+++ BE_Booking/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Ensure this line is correctly placed
+    path('admin/', admin.site.urls),  
     path('api/', include('registeration_app.urls')),  # Include URLs from registration_app
 ]
