2f8e89a004313be8733739261e9d76118cdaca95
Ekamraj Singh <157672776+Ekamraj308@users.noreply.github.com>
2024-04-02 22:43:41 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -20,21 +20,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BE_Booking.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
